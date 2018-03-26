fab11bb595507456abfebcf516fb3fb1ddfb2b00
Diboll <bdiboll@proforma.com>
2018-03-26 18:25:29 -0400

    angularjs functions working

--- public/main.js
+++ public/main.js
@@ -1,5 +1,32 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+angular 
+  .module("scoreBoard", [])
+  .controller("mainController", ($scope) =>{
+   
+    $scope.currentNameOne = " ";
+    $scope.updateName = () => {
+      $scope.currentNameOne = $scope.name;
+    }
 
-document.addEventListener('DOMContentLoaded', main)
+    $scope.currentScoreOne = 0;
+    $scope.addOne = () => {
+      $scope.currentScoreOne = $scope.currentScoreOne + 1;
+    } 
+    $scope.subOne = () => {
+      $scope.currentScoreOne = $scope.currentScoreOne - 1;
+    }
+
+    $scope.currentNameTwo = " ";
+    $scope.updateName2 = () => {
+      $scope.currentNameTwo = $scope.name2;
+    }
+
+    $scope.currentScoreTwo = 0;
+    $scope.addOne2 = () => {
+      $scope.currentScoreTwo = $scope.currentScoreTwo + 1;
+    } 
+    $scope.subOne2 = () => {
+      $scope.currentScoreTwo = $scope.currentScoreTwo - 1;
+    }
+
+  } 
+)
